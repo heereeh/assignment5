e59a5b4cec039670c8f3ce3e4abe934517a6fb2a
heereeh <rodbee20@gmail.com>
2024-02-25 14:24:01 +0900

    gh-pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,43 +4,46 @@ import TV from "./Routes/TV";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-        children: [
-          {
-            path: "movies/:movieId",
-            element: <></>,
-          },
-        ],
-      },
-      {
-        path: "search",
-        element: <Search />,
-        children: [
-          {
-            path: ":type/:id",
-            element: <></>,
-          },
-        ],
-      },
-      {
-        path: "tv",
-        element: <TV />,
-        children: [
-          {
-            path: ":tvId",
-            element: <></>,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+          children: [
+            {
+              path: "movies/:movieId",
+              element: <></>,
+            },
+          ],
+        },
+        {
+          path: "search",
+          element: <Search />,
+          children: [
+            {
+              path: ":type/:id",
+              element: <></>,
+            },
+          ],
+        },
+        {
+          path: "tv",
+          element: <TV />,
+          children: [
+            {
+              path: ":tvId",
+              element: <></>,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: "/assignment5" }
+);
 
 export default router;
